628fe1861e7a08d560fab4447d0d6d4ff7b0e087
binoy-02 <147302748+binoy-02@users.noreply.github.com>
2024-05-11 00:15:51 +0530

    Add files via upload

---
+++ 2DArray.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int r,c;
+	int *arr;
+	int i, j, count = 0;
+	printf("Enter the no of rows and columns:");
+	scanf("%d%d",&r,&c);
+	arr = (int *)malloc(r * c * sizeof(int));
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			*(arr + i*c + j) = ++count;
+		}
+	}
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{	
+			printf("%d\t", *(arr + i*c + j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
