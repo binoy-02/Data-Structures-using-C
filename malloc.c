f997bb4b396bcfda38c72a7d7ef06eadc6984209
binoy-02 <147302748+binoy-02@users.noreply.github.com>
2024-05-14 23:42:58 +0530

    Add files via upload

--- malloc.c
+++ malloc.c
@@ -1,11 +1,25 @@
+//Use malloc to read n integers and find the mean.
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-	int *p;
-	p=(int*)malloc(sizeof(int));
-	printf("enter a number");
-	scanf("%d",p);
-	printf("the value at p is %d",*p);
-	return 0;
+	int *p,n,i,s=0;
+	float mean=0;
+	printf("Number of elements to be entered:");
+	scanf("%d",&n);
+	p=(int *)malloc(sizeof(int)*n);
+	printf("Enter %d numbers:\n",n);
+	for(i=0;i<n;i++)
+	{
+		printf("Enter p[%d]:",i);
+		scanf("%d",p+i);
+	}
+	for(i=0;i<n;i++)
+	{
+		s=s+*(p+i);
+	}
+	mean=s/n;
+	printf("mean:%f",mean);
+	free(p);
 }
